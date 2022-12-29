a0ff4e92e662580e6a381a8423fa1cd4c205e2ba
Leftist Media Group <LeftistMediaGroup@Gmail.com>
2022-12-28 23:18:53 -0500

    Deleted Music file

---
+++ Wade.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import path from 'path';
+import * as fs from 'fs';
+import { dirname } from 'path';
+
+import  * as evs from 'express-video-stream'; // Express Video Stream
+ 
+
+var app = express();
+ 
+evs.addVideo("1", "./Fuck_these_fuckin_fascists.mp4")    //Add video to config
+ 
+app.use(evs.middleware) //Use streaming middleware
+ 
+app.get('/', (req, res) => {
+    var page = fs.readFileSync('./index.html'); // Load html into buffer
+    res.send(page + ' ');
+})
+ 
+app.listen(5004, () => {
+    console.log("Test is up and running on localhost:5004")
+})
