704e596a9a6499333a34abd74ee8c52820da33ce
Daniel Tucker <LeftistMediaGroup@gmail.com>
2023-01-27 12:50:14 -0500

    T

--- Wade.js
+++ Wade.js
@@ -2,4 +2,9 @@ import system from "./modules/system.js";
 import systemSocket from "./modules/systemSocket.js";
 import music from "./modules/music.js";
 
+
+system();
+systemSocket();
+music();
+
 console.log("Wade is online!");
