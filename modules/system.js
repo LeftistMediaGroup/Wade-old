0e6fd6ce7d46440fd7a691ac20a4b79cf2803fb8
Daniel Tucker <LeftistMediaGroup@gmail.com>
2023-01-27 12:27:03 -0500

    T

--- modules/system.js
+++ modules/system.js
@@ -20,70 +20,73 @@ var cors = require('cors');
 
 import registerRouter from './routes/register.js';
 
-var app = express();
-var httpsServer = https.createServer(credentials, app);
+
+export default function system() {
+  var app = express();
+  var httpsServer = https.createServer(credentials, app);
 
 
-// ssl init
-var privateKey = fs.readFileSync('../ssl/wade.pem');
-var certificate = fs.readFileSync('../ssl/wade.pem');
+  // ssl init
+  var privateKey = fs.readFileSync('../ssl/wade.pem');
+  var certificate = fs.readFileSync('../ssl/wade.pem');
 
-var credentials = {key: privateKey, cert: certificate};
+  var credentials = {key: privateKey, cert: certificate};
 
-app.use(cors({
-  origin: true,
-  optionsSuccessStatus: 200,
-  credentials: true
-}));
+  app.use(cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+    credentials: true
+  }));
 
-app.set('port', 4000)
+  app.set('port', 4000)
 
-httpsServer.listen(app.get('port'), () => {
-  console.log(`System is listening on port ${app.get('port')}`);
-});
+  httpsServer.listen(app.get('port'), () => {
+    console.log(`System is listening on port ${app.get('port')}`);
+  });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('trust proxy', 1) // trust first proxy
+  app.set('trust proxy', 1) // trust first proxy
 
-//BodyParser
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+  //BodyParser
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
 
-//Init session
-app.use(session({ 
-  secret: 'keyboard cat',
-  cookie: { maxAge: 60000, httpOnly: true },
-  credentials: true,
-  saveUninitialized: false,
-  resave: true
-}))
+  //Init session
+  app.use(session({ 
+    secret: 'keyboard cat',
+    cookie: { maxAge: 60000, httpOnly: true },
+    credentials: true,
+    saveUninitialized: false,
+    resave: true
+  }))
 
-// Use Router
-app.use("/", registerRouter);
+  // Use Router
+  app.use("/", registerRouter);
 
-app.use(cookieParser());
+  app.use(cookieParser());
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    next(createError(404));
+  });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+};
