dd0bed3b5f2cf1b4292962869821d34f8e7c4d63
Daniel Tucker <LeftistMediaGroup@gmail.com>
2023-01-27 12:58:00 -0500

    T

--- modules/system.js
+++ modules/system.js
@@ -27,8 +27,8 @@ export default function system() {
 
 
   // ssl init
-  var privateKey = fs.readFileSync('../ssl/wade_key.pem');
-  var certificate = fs.readFileSync('../ssl/wade.pem');
+  var privateKey = fs.readFileSync('./ssl/wade_key.pem');
+  var certificate = fs.readFileSync('./ssl/wade.pem');
 
   var credentials = {key: privateKey, cert: certificate};
 
