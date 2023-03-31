2e88eb535c9833d9ff669eb10940d8687b964f26
Daniel Tucker <LeftistMediaGroup@gmail.com>
2023-03-31 19:59:13 -0400

    T

--- modules/system.js
+++ modules/system.js
@@ -72,6 +72,7 @@ export default function system() {
   app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "Back.LeftistMediaGroup.org");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Credentials', true);
     next();
   });
 
