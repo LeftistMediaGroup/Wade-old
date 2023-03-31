dfb8a819357d6711b904b1950b6bb394bb1f7d1b
Daniel Tucker <LeftistMediaGroup@gmail.com>
2023-03-31 19:54:51 -0400

    T

--- modules/system.js
+++ modules/system.js
@@ -67,13 +67,7 @@ export default function system() {
     resave: true
   }))
 
-  app.use(cors());
-
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+  app.use(cors({ origin: true }));
 
   // Use Router
   app.use("/register", registerRouter);
