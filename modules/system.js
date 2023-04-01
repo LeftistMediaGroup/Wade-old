c2e1d632667ec9b377ec8079361530632c51861d
Daniel Tucker <LeftistMediaGroup@gmail.com>
2023-04-01 05:01:33 -0400

    T

--- modules/system.js
+++ modules/system.js
@@ -27,8 +27,12 @@ import musicRouter from './routes/music.js';
 export default function system() {
   var app = express();
 
-  app.use(cors())
-  
+  var corsOptions = {
+    origin: 'http://example.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+  app.use(cors(corsOptions));
+
   var httpsServer = https.createServer(credentials, app);
 
 
