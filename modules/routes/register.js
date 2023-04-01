fe97a4b079f891b2bb27640d8dc3cf34645c36a0
Daniel Tucker <LeftistMediaGroup@gmail.com>
2023-04-01 14:35:06 -0400

    T

--- modules/routes/register.js
+++ modules/routes/register.js
@@ -2,40 +2,40 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
-import express from 'express';
+import express from "express";
 const router = express.Router();
 
-var PouchDB = require('pouchdb');
+var PouchDB = require("pouchdb");
 
-const { v4: uuid_v4 } = require('uuid');
+const { v4: uuid_v4 } = require("uuid");
 
-
-var account_db = new PouchDB('https://Back.LeftistMediaGroup.org/database/account');
+var account_db = new PouchDB(
+  "https://Back.LeftistMediaGroup.org/database/account"
+);
 
 account_db.info().then(function (info) {
-    console.log(info);
-})
+  console.log(info);
+});
 
-router.post('/submit', function (req, res) {
-    let data = req.body;
+router.post("/submit", function (req, res) {
+  let data = req.body;
 
-    data["_id"] = uuid_v4();
+  data["_id"] = uuid_v4();
 
-    console.log(`Data: ${JSON.stringify(data, null, 2)}`);
+  console.log(`Data: ${JSON.stringify(data, null, 2)}`);
 
-    account_db.put(data, function callback(err, result) {
-        if (!err) {
-          console.log(`PUT Result: ${JSON.stringify(result)}`);
-        }
-    });
-
-    account_db.allDocs({
+  account_db.put(data).then(function () {
+    account_db
+      .allDocs({
         include_docs: true,
-      }).then(function (result) {
+      })
+      .then(function (result) {
         console.log(`Final Result: ${JSON.stringify(result, null, 2)}`);
-      }).catch(function (err) {
+      })
+      .catch(function (err) {
         console.log(err);
       });
+  });
 });
 
 export default router;
