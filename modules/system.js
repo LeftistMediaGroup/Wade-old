7b7d039bd56dfe6fb40516c4539fe34c5a727606
Daniel Tucker <LeftistMediaGroup@gmail.com>
2023-03-31 21:08:19 -0400

    T

--- modules/system.js
+++ modules/system.js
@@ -46,12 +46,24 @@ export default function system() {
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'jade');
 
-  app.use(
-    cors({
-      origin: "https://LeftistMediaGroup.org", // restrict calls to those this address
-      methods: "GET, POST, DELETE, PUT"
-    })
-  );
+  app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
   
 
   
