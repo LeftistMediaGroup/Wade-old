4fd9b91d2c39d74f5a650bc306443c5532ac75d8
Daniel Tucker <LeftistMediaGroup@gmail.com>
2023-04-01 23:15:14 -0400

    A Accounts Database

---
+++ modules/accountInit.js
@@ -0,0 +1,30 @@
+// Allow require
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+var PouchDB = require("pouchdb");
+
+var account_db = new PouchDB(
+  "https://Back.LeftistMediaGroup.org/database/account"
+);
+
+account_db.info().then(function () {
+    db.get('Accounts').catch(function (err) {
+        if (err.name === 'not_found') {
+            return {
+                _id: "Accounts",
+                users: {}
+            }
+        }
+    }).then(function (accounts) {
+        console.log(`New Accounts file: ${accounts}`);
+
+        return accounts;
+    }).then(function () {
+        account_db.put(accounts);
+
+        console.log("Account file created");
+    }).catch(function (err) {
+        console.log(err);
+    });
+});
