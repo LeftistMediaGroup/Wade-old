0d0a0ff6784c02cf212c32e3e3810f2d2c7f9ad9
Daniel Tucker <LeftistMediaGroup@gmail.com>
2023-04-02 22:17:48 -0400

    T

--- Wade.js
+++ Wade.js
@@ -3,5 +3,3 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
 import * as express_init from "./modules/expressInit.js";
-
-import * as account_init from "./modules/accountInit.js";
--- modules/expressInit.js
+++ modules/expressInit.js
@@ -12,6 +12,7 @@ var bodyParser = require('body-parser');
 import register from './routes/register.js';
 import database from './routes/database.js';
 
+import {account_init} from "./modules/accountInit.js";
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -26,6 +27,7 @@ app.use(cors({
 
 app.use('/register', register);
 app.use('/database', database);
+account_init.start(app);
 
 app.listen(4000, function () {
   console.log('CORS-enabled web server listening on port 4000')
