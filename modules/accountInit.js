4e1cd471b81e9638c62057bd50d6b791718452c8
Daniel Tucker <LeftistMediaGroup@gmail.com>
2023-04-02 22:12:41 -0400

    T

--- modules/accountInit.js
+++ modules/accountInit.js
@@ -7,24 +7,26 @@ var PouchDB = require("pouchdb");
 
 function start(app){
     new Promise(app.use('/accounts', require('express-pouchdb')(PouchDB))).then(function () {
-        var account_db = new PouchDB('https://Back.LeftistMediaGroup.org/database/account').then(function () {
-            account_db.info().then(function () {
-                account_db.get('User_Accounts',).then(function (result) {
-                    if (result === undefined) {
-                        let doc = {
-                            "_id": 'User_Accounts',
-                            "users": {},
-                            "test": "test"
-                        };
+        setTimeout(() =>{
+            var account_db = new PouchDB('https://Back.LeftistMediaGroup.org/database/account').then(function () {
+                account_db.info().then(function () {
+                    account_db.get('User_Accounts',).then(function (result) {
+                        if (result === undefined) {
+                            let doc = {
+                                "_id": 'User_Accounts',
+                                "users": {},
+                                "test": "test"
+                            };
         
-                        console.log(`Setting Account file: ${JSON.stringify(doc, null, 2)}`);
-                        account_db.put(doc);
-                    } 
-                    else {
-                        console.log(`Returned Account File: ${JSON.stringify(accounts)}`);
-                    }
-                })
-            });
-        });
+                         console.log(`Setting Account file: ${JSON.stringify(doc, null, 2)}`);
+                            account_db.put(doc);
+                        } 
+                        else {
+                            console.log(`Returned Account File: ${JSON.stringify(accounts)}`);
+                        }  
+                    })
+                });
+            });   
+        }, 3000);
     });
 }
