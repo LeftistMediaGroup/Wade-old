a733003dee45ede382dc2d757668670ab77519a8
Daniel Tucker <LeftistMediaGroup@gmail.com>
2023-04-02 22:40:34 -0400

    T

--- modules/accountInit.js
+++ modules/accountInit.js
@@ -5,28 +5,26 @@ const require = createRequire(import.meta.url);
 var PouchDB = require("pouchdb");
 
 
-export function start(app){
+function start(app){
     new Promise(app.use('/accounts', require('express-pouchdb')(PouchDB))).then(function () {
-        setTimeout(() =>{
-            var account_db = new PouchDB('https://Back.LeftistMediaGroup.org/database/account').then(function () {
-                account_db.info().then(function () {
-                    account_db.get('User_Accounts',).then(function (result) {
-                        if (result === undefined) {
-                            let doc = {
-                                "_id": 'User_Accounts',
-                                "users": {},
-                                "test": "test"
-                            };
+        var account_db = new PouchDB('https://Back.LeftistMediaGroup.org/database/account').then(function () {
+            account_db.info().then(function () {
+                account_db.get('User_Accounts',).then(function (result) {
+                    if (result === undefined) {
+                        let doc = {
+                            "_id": 'User_Accounts',
+                            "users": {},
+                            "test": "test"
+                        };
         
-                         console.log(`Setting Account file: ${JSON.stringify(doc, null, 2)}`);
-                            account_db.put(doc);
-                        } 
-                        else {
-                            console.log(`Returned Account File: ${JSON.stringify(accounts)}`);
-                        }  
-                    })
-                });
-            });   
-        }, 3000);
+                        console.log(`Setting Account file: ${JSON.stringify(doc, null, 2)}`);
+                        account_db.put(doc);
+                    } 
+                    else {
+                        console.log(`Returned Account File: ${JSON.stringify(accounts)}`);
+                    }
+                })
+            });
+        });
     });
 }
