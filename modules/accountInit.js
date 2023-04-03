29a91c332c24e0b0dc234e567a05f98c381c392c
Daniel Tucker <LeftistMediaGroup@gmail.com>
2023-04-02 22:07:24 -0400

    T

--- modules/accountInit.js
+++ modules/accountInit.js
@@ -4,24 +4,27 @@ const require = createRequire(import.meta.url);
 
 var PouchDB = require("pouchdb");
 
-var account_db = new PouchDB(
-  "User_accounts"
-);
 
-account_db.info();
-
-account_db.get('User_Accounts', {attachments: true}).catch(function (err) {
-    if (err.name === 'not_found') {
-        let doc = {
-            "_id": 'User_Accounts',
-            "users": {},
-            "test": "test"
-        };
-
-        console.log(`Setting Account file: ${JSON.stringify(doc, null, 2)}`);
-        account_db.put(doc);
-    } 
-}).then(function (accounts) {
-    console.log(`Returned Account File: ${JSON.stringify(accounts)}`);
-});
-
+function start(app){
+    new Promise(app.use('/accounts', require('express-pouchdb')(PouchDB))).then(function () {
+        var account_db = new PouchDB('https://Back.LeftistMediaGroup.org/database/account').then(function () {
+            account_db.info().then(function () {
+                account_db.get('User_Accounts',).then(function (result) {
+                    if (result === undefined) {
+                        let doc = {
+                            "_id": 'User_Accounts',
+                            "users": {},
+                            "test": "test"
+                        };
+        
+                        console.log(`Setting Account file: ${JSON.stringify(doc, null, 2)}`);
+                        account_db.put(doc);
+                    } 
+                    else {
+                        console.log(`Returned Account File: ${JSON.stringify(accounts)}`);
+                    }
+                })
+            });
+        });
+    });
+}
