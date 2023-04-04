a8171903f8f51747a5649c76edebbafbec59dbdb
Daniel Tucker <LeftistMediaGroup@gmail.com>
2023-04-04 16:37:19 -0400

    A .env

--- modules/expressInit.js
+++ modules/expressInit.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json())
 
 app.use(cors({
   credentials: true,
-  origin: "https://leftistmediagroup.org"
+  origin: `${process.env.backend}`
 }));
 
 app.use('/register', register);
