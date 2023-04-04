ddd83d98b599a8ac6ce01529f3456ebdc4b7e2e3
Daniel Tucker <LeftistMediaGroup@gmail.com>
2023-04-04 17:50:28 -0400

    T

--- modules/accountInit.js
+++ modules/accountInit.js
@@ -10,20 +10,24 @@ var account_db = new PouchDB(
   `https://${process.env.backend}/database/account`
 );
 
-setTimeout(()=> {
-    account_db.info().then(function () {
-        account_db.get('User_Accounts').catch(function (err) {
-            if (err.name === 'not_found') {
-                let doc = {
-                    "_id": 'User_Accounts',
-                    "users": {}
-                };
-        
-                console.log(`Setting Account file: ${JSON.stringify(doc, null, 2)}`);
-                account_db.put(doc);
-            } 
-        }).then(function (accounts) {
-            console.log(`Returned Account File: ${JSON.stringify(accounts)}`)
+try {
+    setTimeout(()=> {
+        account_db.info().then(function () {
+            account_db.get('User_Accounts').catch(function (err) {
+                if (err.name === 'not_found') {
+                    let doc = {
+                        "_id": 'User_Accounts',
+                        "users": {}
+                    };
+            
+                    console.log(`Setting Account file: ${JSON.stringify(doc, null, 2)}`);
+                    account_db.put(doc);
+                } 
+            }).then(function (accounts) {
+                console.log(`Returned Account File: ${JSON.stringify(accounts)}`)
+            });
         });
-    });
-}, 4000);
+    }, 4000);
+} catch (err) {
+    console.log(`Error: ${JSON.stringify(err, null, 2)}`);
+};
