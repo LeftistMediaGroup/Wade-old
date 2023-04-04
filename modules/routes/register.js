42e2663886dca69357dc1af8a759dd3a9eb7da0d
Daniel Tucker <LeftistMediaGroup@gmail.com>
2023-04-04 18:04:58 -0400

    T

--- modules/routes/register.js
+++ modules/routes/register.js
@@ -13,36 +13,36 @@ const set = require('set-value');
 
 require('dotenv').config()
 
-try{
+setTimeout(() => {
   var account_db = new PouchDB(
     `https://${process.env.backend}/database/account`
   );
-} catch (err){
-  console.log(`Error: ${err}`);
-};
 
-account_db.info()
+  account_db.info()
 
-router.post("/submit", function (req, res) {
-  let user = req.body;
-  
-  console.log(`User in: ${JSON.stringify(user, null, 2)}`);
-
-  user["_id"] = uuid_v4();
-
-  console.log(`User modified: ${JSON.stringify(user, null, 2)}`);
-
-  account_db.get('User_Accounts').then(function (accounts) {
-    console.log(`Account file in ${JSON.stringify(accounts, null, 2)}`);
-
-    set(accounts, `users.${user.email}`, user);
-
-    console.log(`Accounts file out: ${JSON.stringify(accounts, null, 2)}`);
+  router.post("/submit", function (req, res) {
+    let user = req.body;
     
-    account_db.put(accounts);
-  }).catch(function (err) {
-    console.log(`Error: ${JSON.stringify(err, null, 2)}`);
+    console.log(`User in: ${JSON.stringify(user, null, 2)}`);
+
+    user["_id"] = uuid_v4();
+
+    console.log(`User modified: ${JSON.stringify(user, null, 2)}`);
+
+    account_db.get('User_Accounts').then(function (accounts) {
+      console.log(`Account file in ${JSON.stringify(accounts, null, 2)}`);
+
+      set(accounts, `users.${user.email}`, user);
+
+      console.log(`Accounts file out: ${JSON.stringify(accounts, null, 2)}`);
+      
+      account_db.put(accounts);
+    }).catch(function (err) {
+      console.log(`Error: ${JSON.stringify(err, null, 2)}`);
+    })  ;
   });
-});
+}, 4000);
+
+
 
 export default router;
