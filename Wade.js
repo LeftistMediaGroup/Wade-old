993226c4b2f6cff26c33e3efce99092dd167f7b2
Daniel Tucker <DanielATucker@Outlook.com>
2023-05-19 23:58:27 -0400

    Updated Files

--- Wade.js
+++ Wade.js
@@ -1,7 +1,25 @@
-// Allow require
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
+import { Express_Init_Start } from "./modules/express_init.js";
+import { Database_init_start } from "./modules/database_init.js";
 
-import * as express_init from "./modules/expressInit.js";
 
-//import * as account_init from "./modules/accountInit.js";
+function Start() {
+    new Promise(() => {
+        try {
+            let value = Express_Init_Start();
+            resolveExpressInit(value);
+        } catch (err) {
+            rejectExpressInit(err)
+        };
+    });
+
+
+    function resolveExpressInit() {
+        Database_init_start();
+    }
+
+    function rejectExpressInit(err) {
+        console.log(`Error: ${JSON.stringify(err, null, 2)}`);
+    }
+};
+
+Start();
