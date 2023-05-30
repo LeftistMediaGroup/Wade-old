84f019e1157dc381f0e8f58ff34023fa1cf20a40
Leftist Media Group <LeftistMediaGroup@Gmail.com>
2023-05-30 18:24:07 -0400

    T 230530 Updated files Not tested

---
+++ modules/routes/music.js
@@ -0,0 +1,27 @@
+// Allow require
+import { createRequire } from "module";
+
+import express from 'express';
+import  * as evs from 'express-video-stream'; // Express Video Stream
+
+
+const require = createRequire(import.meta.url);
+
+
+const fs = require('fs')
+const path = require('path')
+
+
+
+var router = express.Router();
+
+router.get('/', (req, res) => { 
+    try {
+        var page = fs.readFileSync("./modules/routes/index.html"); // Load html into buffer
+        res.send(page + ' ');
+    } catch (err) {
+        console.log(`Error: ${err}`);
+    };
+});
+
+export default router;
