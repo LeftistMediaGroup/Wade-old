c0943ec63f353cf513b10b56004f6888ca4828a4
Leftist Media Group <LeftistMediaGroup@Gmail.com>
2023-06-27 13:23:18 +0000

    Jun 27, 2023, 9:23 AM

--- modules/RSS/rss.js
+++ modules/RSS/rss.js
@@ -1,3 +1,11 @@
+// Allow require
+import { createRequire } from "module";
+
+//End require
+const require = createRequire(import.meta.url);
+
+var PouchDB = require("pouchdb");
+
 import Parser from "rss-parser";
 let parser = new Parser();
 
@@ -5,10 +13,19 @@ async function RefreshFeed() {
   console.log(`Refreshing RSS feed`);
 
   let feed = await parser.parseURL("https://www.reddit.com/.rss");
-  
+
   console.log(`Feed: ${feed.title} Refreshed`);
 }
 
-export default function startRSS(){
-    RefreshFeed();
+function getFeed() {
+  var RSS_db = new PouchDB(
+    `https://${process.env.host}:${process.env.port}/database/rss`
+  );
+  RSS_db.info().then(function (info) {
+    console.log(`RSS From Database: ${JSON.stringify(info)}`);
+  });
+}
+
+export default function startRSS() {
+  RefreshFeed();
 }
