ce699882bff4f5e12a8b248fc29c88b43d1bf4a6
Leftist Media Group <LeftistMediaGroup@Gmail.com>
2023-06-27 23:04:29 +0000

    Jun 27, 2023, 7:04 PM

--- modules/RSS/rss.js
+++ modules/RSS/rss.js
@@ -9,33 +9,65 @@ var PouchDB = require("pouchdb");
 const RssFeedEmitter = require("rss-feed-emitter");
 const feeder = new RssFeedEmitter();
 
-async function RefreshFeed() {
-  console.log(`Refreshing RSS feed \n`);
-
-  feeder.add({
-    url: ["https://www.reddit.com/.rss", "https://theanarchistlibrary.org/feed"],
-    refresh: 2000,
-  });
-
-  feeder.on("new-item", function (item) {
-    console.log(`Item: ${JSON.stringify(item, null, 2)}`);
-  });
-
-  feeder.on("error", console.error);
-
-  //feeder.list;
-}
-
-function getFeed() {
-  var RSS_db = new PouchDB(
-    `https://${process.env.host}:${process.env.port}/database/rss`
-  );
-  RSS_db.info().then(function (info) {
-    console.log(`RSS From Database: ${JSON.stringify(info)}`);
-  });
-}
-
 export default async function startRSS() {
-  console.log(`Starting RSS Server`);
-  await RefreshFeed();
+  console.log(`Starting RSS Server\n`);
+  let FeedStore = new feedStore();
+}
+
+class feedStore {
+  constructor() {
+    this.getFeed();
+  }
+
+  getFeed() {
+    var RSS_db = new PouchDB(
+      `https://${process.env.host}:${process.env.port}/database/rss`
+    );
+
+    RSS_db.info().then(function (info) {
+      console.log(`RSS From Database: ${JSON.stringify(info)}`);
+
+      console.log(`Refreshing RSS feed \n`);
+
+      feeder.add({
+        url: [
+          "https://www.reddit.com/.rss",
+          "https://theanarchistlibrary.org/feed",
+          "https://itsgoingdown.org/feed",
+          "https://freedomnews.org.uk/feed",
+          "https://anarchistnews.org/rss.xml",
+        ],
+        refresh: 2000,
+      });
+
+      function newItem(feeder, info) {
+        feeder.on("new-item", function (item) {
+          let exists = Object.values(info).includes(item.title);
+
+          if (exists === false) {
+            try {
+              RSS_db.put({
+                _id: item.title,
+                body: { item },
+              }).catch((err) => {
+                console.log(`Error: ${err}`);
+              });
+            } catch (err) {
+              console.log(`Error: ${err}`);
+            }
+
+            console.log(`Item Inserted: ${item.title}\n`);
+          } else {
+            console.log(`Item Found: ${item.title}\n`);
+          }
+        });
+      }
+
+      newItem(feeder, info);
+
+      feeder.on("error", console.error);
+
+      //feeder.list;
+    });
+  }
 }
