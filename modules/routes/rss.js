91b0b0eeddfeb1abb83af831cb1b1d062fe7b14a
Leftist Media Group <LeftistMediaGroup@Gmail.com>
2023-06-28 14:38:26 +0000

    Jun 28, 2023, 10:38 AM

--- modules/routes/rss.js
+++ modules/routes/rss.js
@@ -1,29 +1,38 @@
 // Allow require
 import { createRequire } from "module";
 
-import { v4 as uuidv4 } from 'uuid';
-import * as dotenv from 'dotenv';
+import { v4 as uuidv4 } from "uuid";
+import * as dotenv from "dotenv";
 
 //End require
 const require = createRequire(import.meta.url);
 var PouchDB = require("pouchdb");
 
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var strftime = require('strftime') 
+var strftime = require("strftime");
 
 dotenv.config();
-  
 
-router.get('/get_rss', (req, res) => {
-  var RSS_db = new PouchDB(`https://${process.env.host}:${process.env.port}/database/rss`);
-      
+router.get("/get_rss", (req, res) => {
+  var RSS_db = new PouchDB(
+    `https://${process.env.host}:${process.env.port}/database/rss`
+  );
+
   RSS_db.info().then(function () {
-    RSS_db.get("Calendar").then(function (result) {
-      res.json(result);
-      res.end();
-    });
+    RSS_db.allDocs({
+      include_docs: true,
+      attachments: true,
+    })
+      .then(function (result) {
+        res.json(result);
+        res.end();
+      })
+      .catch(function (err) {
+        res.json(err);
+        res.end();
+      });
   });
 });
 
