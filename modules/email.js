371ad3344fb1e327aec6746584816c3dbe4699d4
Daniel Tucker <DanielATuker@Outlook.com>
2023-09-02 19:11:25 -0400

    A Email

---
+++ modules/email.js
@@ -0,0 +1,73 @@
+// Allow require
+import { createRequire } from "module";
+
+import * as dotenv from "dotenv";
+
+//End require
+const require = createRequire(import.meta.url);
+
+
+const nodemailer = require("nodemailer");
+
+export default class Send_Mail {
+
+    constructor(data) {
+        this.transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 587,
+            auth: {
+              user: `${process.env.gmail_user}`,
+              pass: `${process.env.gmail_pass}`,
+            },
+        });
+
+        this.data = data;
+    };
+
+    Message = () => {
+        return(
+            <img
+            src={this.data.alias.avatar}/>
+            <p>
+                Welcome {this.data.alias.first}, {this.data.alias.last}!
+            </p>
+            <br/>
+
+            <p>Wait, that's not your name! It's your Alias!</p>
+            <br/>
+
+            <p>
+                You've just completed the first step to working with LMG, and that's registering on our website, and with that comes a brand new name to keep the bad-actors from finding out who you are!
+            </p>
+            <br/>
+
+            <p>
+                Evreything we do is anonymous to the best of our abilities! 
+            </p>
+            < br/>
+
+            <p> Our admins have been notified of your new user creation, they will be reaching out via email soon.</p>
+            <p> If you'd like to talk directly to an admin, please respond to this email. </p>
+            <br/>
+            <b>This email IS monitored</b>
+
+            <b>Join our Discord for LMG (Community Support Network)</b>
+            <br/>
+            <b>https://discord.gg/SSZhgF5kWB</b>
+
+            <p>This email was sent by Leftist Media Group's automated system, Wade.</p>
+        )
+    };
+
+
+  transporter.verify().then(console.log).then(() => {
+    transporter.sendMail({
+        from: '"Leftist Media Group - Wade" <LeftistMediaGroup@gmail.com>', // sender address
+        to: `LeftistMediaGroup@Gmail.com, ${this.data.email}`, // list of receivers
+        subject: "New User Account!", // Subject line
+        html: this.Message(),
+      }).then(info => {
+        console.log({info});
+      }).catch(console.error);
+  }).catch(console.error);
+}
--- modules/routes/system.js
+++ modules/routes/system.js
@@ -2,23 +2,36 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
+var Chance = require('chance');
+var chance = new Chance();
+
 var PouchDB = require("pouchdb");
 
 var express = require('express');
 var router = express.Router();
 
 import * as dotenv from 'dotenv';
+import strftime from "strftime";
+import Send_Mail from "../email";
 
 dotenv.config();
   
-router.post('/register_admin', (req, res) => {
+router.post('/register_user', (req, res) => {
   try {
     let username = req.body.username;
     let password = req.body.password;
+    let email = req.body.email;
     
     let data = {
       "username": username,
-      "password": password
+      "password": password,
+      "email": email,
+      "registerTime": strftime("%Y%M%D_%X"),
+      "alias": {
+        "first": chance.first(),
+        "last": chance.last(),
+        "avatar": chance.avatar()
+      }
     };
   
     console.log(`Data: ${JSON.stringify(data, null, 2)}`)
@@ -44,6 +57,10 @@ router.post('/register_admin', (req, res) => {
             req.session.save()
             res.send("Success");
             res.end();
+
+            //Send emails
+            Send_Mail(data);
+
           }).catch(function (err) {
             console.log(`Error: ${err}`);
           });
