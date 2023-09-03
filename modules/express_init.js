3f651ff87494a783a1db6ba597804993366d2bfc
Daniel Tucker <DanielATuker@Outlook.com>
2023-09-02 21:13:26 -0400

    R Cookie middleware

--- modules/express_init.js
+++ modules/express_init.js
@@ -68,28 +68,6 @@ export function Express_Init_Start() {
     })
   );
 
-  // set a cookie
-  app.use(function (req, res, next) {
-    // check if client sent cookie
-    var cookie = req.cookies.cookieName;
-    if (cookie === undefined) {
-      // no: set a new cookie
-      var randomNumber = Math.random().toString();
-      randomNumber = randomNumber.substring(2, randomNumber.length);
-      res.cookie("cookieName", randomNumber, {
-        maxAge: 900000,
-        httpOnly: true,
-        sameSite: "none",
-        secure: true
-      });
-      console.log("cookie created successfully");
-    } else {
-      // yes, cookie was already present
-      console.log("cookie exists", cookie);
-    }
-    next(); // <-- important!
-  });
-
   // parse application/x-www-form-urlencoded
   app.use(bodyParser.urlencoded({ extended: false }));
 
