a18a81b5c199ae586687d3412dff60d60c14b6a8
Daniel Tucker <DanielATuker@Outlook.com>
2023-09-07 16:34:41 -0400

    U Cors

--- modules/express_init.js
+++ modules/express_init.js
@@ -65,7 +65,7 @@ export function Express_Init_Start() {
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false, sameSite: "none", maxAge: 86400000, httpOnly: false }
+    cookie: { secure: true, sameSite: "none", maxAge: 86400000, httpOnly: false }
   }))
 
   app.use(cookieParser());
